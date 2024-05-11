6b7ed987296837888418c194de6ac48ce9ba947c
Alpyg <alpyg@pm.me>
2024-05-11 00:24:42 -0400

    Updated packages

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-#![cfg_attr(feature = "bench", feature(test))]
 #![cfg_attr(feature = "gat", feature(generic_associated_types))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
 
-#[cfg(all(test, feature = "std", feature = "bench"))]
+#[cfg(all(test, feature = "std"))]
 extern crate test;
 
 mod deserialize;
